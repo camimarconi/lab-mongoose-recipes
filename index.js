11d30c04ddec08fec2e3c710ed706a7de0a1d349
Camila Solér <msoler.camila@gmail.com>
2022-05-06 22:12:53 -0300

    iteration 1 done, but error at connection to my BD

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+
+async function init() {
+  try {
+    const connection = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Conectado ao banco", connection.connections[0].name);
+
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
+    // return Recipe.deleteMany();
+    //***************ATENÇAO - LIMPAR NOSSO BANCO DE DADOS  */
+    connection.connections[0].dropDatabase();
+
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    // Iteration 2 - Create a recipe
+    const recipeCreated = recipeSchema.create({
+      title: "Chocolate Cake",
+      level: "Easy Peasy",
+      ingredients:
+        "all-purpose flour, sugar, unsweetened cocoa powder, baking powder, baking soda, salt, espresso powder, milk, oil, eggs, vanilla extract and boiling water",
+      cuisine: "Worldwide",
+      dishType: "dessert",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 60,
+      creator: "Camila",
+      created: "06/05/2022",
+    });
+    console.log("recipeCreated", recipeCreated);
+  } catch (error) {
+    console.error("Error connecting to the database", error);
+  }
+}
+
+init();
